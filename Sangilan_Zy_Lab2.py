190cbdd9720c7d911fb9eec3cc97f941bab81afd
Szygzy <90661321+Blurf-uwu@users.noreply.github.com>
2026-08-24 11:34:58 +0800

    Update Sangilan_Zy_Lab2.py

--- Sangilan_Zy_Lab2.py
+++ Sangilan_Zy_Lab2.py
@@ -1,59 +1,37 @@
+def inforloop(a, b, c, d=1):
+    numbers = range(b,c,d)
+    for number in numbers:
+        print(number)
+    print(a in numbers)
+    print(" ")
+
+
 # (1) 14 in range(0, 2)
-for i in range(0, 2):
-    print(i)
-print(14 in range(0, 2))        # No
-print(" ")
+inforloop(14, 0, 2)
 
 # (2) 14 in range(0, -2)
-for i in range(0, -2):
-    print(i)
-print(14 in range(0, -2))       # No
-print(" ")
+inforloop(14, 0, -2)
 
 # (3) 14 in range(0, 100, 2)
-for i in range(0, 100, 2):
-    print(i)
-print(14 in range(0, 100, 2))   # Yes
-print(" ")
+inforloop(14, 0, 100, 2)
 
 # (4) -14 in range(0, 100, -2)
-for i in range(0, 100, -2):
-    print(i)
-print(-14 in range(0, 100, -2)) # No
-print(" ")
+inforloop(-14, 0, 100, -2)
 
 # (5) 5 in range(5, -5, -5)
-for i in range(5, -5, -5):
-    print(i)
-print(5 in range(5, -5, -5))    # Yes
-print(" ")
+inforloop(5, 5, -5, -5)
 
 # (6) 0 in range(5, -5, -5)
-for i in range(5, -5, -5):
-    print(i)
-print(0 in range(5, -5, -5))    # Yes
-print(" ")
+inforloop(0, 5, -5, -5)
 
 # (7) -5 in range(5, -5, -5)
-for i in range(5, -5, -5):
-    print(i)
-print(-5 in range(5, -5, -5))   # No
-print(" ")
+inforloop(-5, 5, -5, -5)
 
 # (8) 70 in range(-111, 111, 3)
-for i in range(-111, 111, 3):
-    print(i)
-print(70 in range(-111, 111, 3))  # No
-print(" ")
+inforloop(70, -111, 111, 3)
 
 # (9) 71 in range(-111, 111, -3)
-for i in range(-111, 111, -3):
-    print(i)
-print(71 in range(-111, 111, -3)) # No
-print(" ")
+inforloop(71, -111, 111, -3)
 
 # (10) 72 in range(111, -111, -3)
-for i in range(111, -111, -3):
-    print(i)
-print(72 in range(111, -111, -3)) # Yes
-print(" ")
+inforloop(72, 111, -111, -3)
